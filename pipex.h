b748c6181c6f501472e1a9951e9261ff72342dc7
chuchilini <vanesalees@gmail.com>
2023-02-17 16:05:10 +0200

    --all

---
+++ pipex.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoonslee <yoonslee@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/17 14:59:12 by yoonslee          #+#    #+#             */
+/*   Updated: 2023/02/17 15:07:41 by yoonslee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>//perror, strerror
+# include <sys/wait.h>//wait, waitpid
+
+
+
+
+
+
+#endif
