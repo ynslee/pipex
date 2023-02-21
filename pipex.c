1b370aefb167c103a4e267652be7075838fa3df9
chuchilini <vanesalees@gmail.com>
2023-02-21 14:01:19 +0200

    --all

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: yoonslee <yoonslee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 14:59:01 by yoonslee          #+#    #+#             */
-/*   Updated: 2023/02/21 11:27:24 by yoonslee         ###   ########.fr       */
+/*   Updated: 2023/02/21 14:00:51 by yoonslee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,12 @@
 1. First check if the argument count 
 2. check if every argv is correct
 */
+int argv_is_correct(char **argv,t_pipex *pipex)
+{
+	pipex.fd[0] = open(argv[1], O_RDONLY);
+		perror("failed to open file")
+	
+}
 
 int	main(int argc, char *argv[], char **env)
 {
@@ -23,8 +29,9 @@ int	main(int argc, char *argv[], char **env)
 
 	if (argc != 5)
 	{
-		write(1, "Wrong number of arguments!\n", 27);
+		write(1, "arguments number is incorrect!\n", 31);
 		exit(1);
 	}
-	argv_is_correct(char *argv)
+	argv_is_correct(argv, pipex);
+		
 }
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: yoonslee <yoonslee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 14:59:12 by yoonslee          #+#    #+#             */
-/*   Updated: 2023/02/21 11:05:05 by yoonslee         ###   ########.fr       */
+/*   Updated: 2023/02/21 13:41:05 by yoonslee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ typedf	strcut s_pipex{
 	int 	fd[2];
 }			t_pipex;
 
-
+void argv_iscorrect(t_pipex *pipex, char **argv)
 
 
 #endif
